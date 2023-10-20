96a4bbad2d56f8e52d61443e6b406faaaa499573
Dwayne Macgowan <dwaynemac@gmail.com>
2023-10-20 17:21:29 -0300

    dont color slashed

--- railway.plugin.zsh
+++ railway.plugin.zsh
@@ -1,6 +1,6 @@
 function railway_prompt_info() {
     if [ -n "$RAILWAY_PROJECT_NAME" ] || [ -n "$RAILWAY_ENVIRONMENT_NAME" ] || [ -n "$RAILWAY_SERVICE_NAME" ]; then
-      echo "railway:(%{$fg[red]%}${RAILWAY_PROJECT_NAME}/${RAILWAY_ENVIRONMENT_NAME}/${RAILWAY_SERVICE_NAME}%{$reset_color%})"
+      echo "railway:(%{$fg[red]%}${RAILWAY_PROJECT_NAME}%{$reset_color%}/%{$fg[red]%}${RAILWAY_ENVIRONMENT_NAME}%{$reset_color%}/%{$fg[red]%}${RAILWAY_SERVICE_NAME}%{$reset_color%})"
     fi
 }
 
