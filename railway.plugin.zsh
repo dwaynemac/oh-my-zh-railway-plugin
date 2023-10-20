06470db4c1f5b26280ac7894118e1834e703d907
Dwayne Macgowan <dwaynemac@gmail.com>
2023-10-20 17:14:48 -0300

    mal el comment

--- railway.plugin.zsh
+++ railway.plugin.zsh
@@ -1,5 +1,4 @@
 function railway_prompt_info() {
-    # Check if RAILWAY_ENVIRONMENT_NAME and RAILWAY_PROJECT_NAME and RAILWAY_SERVICE_NAME are set and not empty
     if [ -n "$RAILWAY_PROJECT_NAME" ] || [ -n "$RAILWAY_ENVIRONMENT_NAME" ] || [ -n "$RAILWAY_SERVICE_NAME" ]; then
       echo "railway:(%{$fg[red]%}${RAILWAY_PROJECT_NAME}/${RAILWAY_ENVIRONMENT_NAME}/${RAILWAY_SERVICE_NAME}%{$reset_color%})"
     fi
